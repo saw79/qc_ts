08ac336df3552bd76a6086b9f7e85b762168a65c
Steve Wacks <swacks@gmail.com>
2020-08-27 06:58:54 -0400

    multiplied w/h by DPR

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ import {NinePatchPlugin} from "@koreez/phaser3-ninepatch";
 
 const config = {
   title: "Quantum Cortex",
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: window.innerWidth*window.devicePixelRatio,
+  height: window.innerHeight*window.devicePixelRatio,
   parent: "game",
   scale: {
     mode: Phaser.Scale.FIT,
